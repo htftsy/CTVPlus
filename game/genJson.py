f7512adf9642f1211711f0953fd6f9c03e390777
htf <htftsy@gmail.com>
2025-09-27 00:21:20 +0800

    Basics

--- game/genJson.py
+++ game/genJson.py
@@ -1,7 +1,69 @@
 import json
 
 rhsCase0 = {
-	
+	"op": "Band",
+	"lhs": {
+		"op": "Band",
+		"lhs": { # BRI0 [1]
+		},
+		"rhs": { # BRI1 [1]
+		}
+	},
+	"rhs": {
+		"op": "Band",
+		"lhs": { # Ver(y_0, sig_0)
+		},
+		"rhs": { # oup_2 bot
+		}
+	}
+}
+
+rhsCase1 = {
+	"op": "Band",
+	"lhs": {
+		"op": "Band",
+		"lhs": { # BRI0 [2]
+		},
+		"rhs": { # Ver(y_0, sig_0)
+		}
+	},
+	"rhs": { # oup_1 bot
+	}
+}
+
+CorrWin = {
+}
+
+rhsCase2 = {
+	"op": "Band",
+	"lhs": {
+		"op": "Band",
+		"lhs": {
+			"op": "Band",
+			"lhs": { # Ver(y_0, sig_0)
+			},
+			"rhs": { # inpRI 0 [2]
+			}
+		},
+		"rhs": {
+			"op": "Band",
+			"lhs": { # inpRI 1 [2]
+			},
+			"rhs": { # inp_2 bot
+			}
+		}
+	},
+	"rhs": {
+		"op": "Band",
+		"lhs": { # oup_1 bot
+			"op": "Band",
+			"lhs": CorrWin,
+			"rhs": { # oup_0 out_1 = 0
+			}
+		},
+		"rhs": {
+		}
+	}
 }
 
 final = {
